f44f9a4c5e859fefedfe3799053af582dab01ef4
swoutch <38116812+swoutch@users.noreply.github.com>
2022-02-22 01:22:21 +0100

    Rename resources

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,5 +1,5 @@
 import os
-import time
+from datetime import datetime, timezone
 
 import pulumi
 from pulumi_gcp import cloudfunctions, storage
@@ -9,7 +9,7 @@ from pulumi_gcp import cloudfunctions, storage
 PATH_TO_SOURCE_CODE = "../src"
 
 # We will store the source code to the Cloud Function in a Google Cloud Storage bucket.
-bucket = storage.Bucket("source-code", location="US-EAST1")
+bucket = storage.Bucket("cloud_functions_source_code", location="US-EAST1", force_destroy=True)
 
 # The Cloud Function source code itself needs to be zipped up into an
 # archive, which we create using the pulumi.AssetArchive primitive.
@@ -25,8 +25,8 @@ archive = pulumi.AssetArchive(assets=assets)
 # The timestamp is incorporated in the name because if the name does not
 # change, the function does not detect change of the source code
 source_archive_object = storage.BucketObject(
-    "eta_demo_object",
-    name="main.py-%f" % time.time(), #TODO: iso datetime instead of unix ts, or maybe hash
+    "tell_time_source_archive",
+    name=f"tell_time_source-{datetime.now(tz=timezone.utc)}", #TODO: use hash instead of datetime to avoid unecessary deploys
     bucket=bucket.name,
     source=archive)
 
@@ -34,10 +34,13 @@ source_archive_object = storage.BucketObject(
 # Cloud Storage.
 #TODO: make public
 fxn = cloudfunctions.Function(
-    "eta_demo_function",
+    "tell_time_cloud_function",
     entry_point="tell_time",
-    region="us-central1", #TODO: mettre une region gratuite
+    region="us-east1", #TODO: mettre une region gratuite
     runtime="python37",
+    available_memory_mb=128,
     source_archive_bucket=bucket.name,
     source_archive_object=source_archive_object.name,
     trigger_http=True)
+
+#TODO: manage google generated buckets: http://www.theappliedarchitect.com/what-the-heck-are-these-cloud-storage-buckets/
